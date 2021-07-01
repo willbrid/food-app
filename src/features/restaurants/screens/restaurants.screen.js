a9f6bd523ed55afaafabd60372463e346afc82a9
william bridge <ngaswilly77@gmail.com>
2021-07-01 06:05:11 +0200

    feat: add flatlist design

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,17 +1,31 @@
 import React, { useContext } from "react";
-import { View, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { View, SafeAreaView, StatusBar, StyleSheet, FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
 
 import { ThemeContext } from "../../../providers/theme/theme.provider";
 
 import RestaurantInfoCard from "../components/restaurant-info-card.component";
+import Spacer from "../../../components/spacer/spacer.component";
 
 // Compartibilité entre andoird et ios pour StatusBar.currentHeight
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px;`}
 `;
+
+const data = [
+  { name: 1 },
+  { name: 2 },
+  { name: 3 },
+  { name: 4 },
+  { name: 5 },
+  { name: 6 },
+  { name: 7 },
+  { name: 8 },
+  { name: 9 },
+  { name: 10 }
+];
 
 const RestaurantsScreen = () => {
   const theme = useContext(ThemeContext);
@@ -21,9 +35,13 @@ const RestaurantsScreen = () => {
       <View style={styles(theme).searchContainer}>
         <Searchbar />
       </View>
-      <View style={styles(theme).restaurantListContainer}>
-        <RestaurantInfoCard />
-      </View>
+      <FlatList 
+          data={data}
+          renderItem={() => <RestaurantInfoCard />}
+          keyExtractor={(item) => item.name.toString()}
+          contentContainerStyle={{ padding: theme.space[3] }}
+          ItemSeparatorComponent={() => <Spacer size="large" />}
+      />
     </SafeArea>
   );
 };
