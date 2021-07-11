140aaf17c477488608de5220402f0c9cd2ff0aa5
william bridge <ngaswilly77@gmail.com>
2021-07-11 22:07:02 +0200

    refactor: update safe-area and title components

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -1,10 +1,24 @@
-import { SafeAreaView, StatusBar } from "react-native";
-import styled from "styled-components/native";
+import React, { useContext } from 'react';
+import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
 
-// Compartibilité entre andoird et ios pour StatusBar.currentHeight
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px;`}
-`;
+import { ThemeContext } from '../../providers/theme/theme.provider';
+
+const SafeArea = ({ children }) => {
+    const theme = useContext(ThemeContext);
+
+    return (
+        <SafeAreaView style={styles(theme).container}>
+          {children}
+        </SafeAreaView>
+    );
+};
+
+const styles = (theme) => StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight ? StatusBar.currentHeight : 0, // Compatibilité entre andoird et ios pour StatusBar.currentHeight
+      backgroundColor: theme.colors.bg.primary
+    }
+});
 
 export default SafeArea;
--- src/features/account/components/title.component.js
+++ src/features/account/components/title.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const Title = ({ content }) => (
-    <Text style={{fontSize: 30}}>{content}</Text>  
+    <Text style={{fontSize: 30, fontWeight: 'bold'}}>{content}</Text>  
 );
 
 export default Title;
